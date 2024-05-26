e761cc9062a5cc22a3456858fa68cbb61ec2969a
Abdo <ba2662003@gmail.com>
2024-05-26 13:51:20 +0300

    create spinner product

--- src/components/localProduct.tsx
+++ src/components/localProduct.tsx
@@ -26,11 +26,19 @@ const LocalProduct = () => {
         <div>
             {
                 user.email ?
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-10 sm:px-20">
+                <>
                     {
-                        products?.map((ele:IProp)=> <ShowProduct product={ele} key={ele.id}/>)
+                        products.length > 0 ? 
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-10 sm:px-20">
+                        {
+                            products?.map((ele:IProp)=> <ShowProduct product={ele} key={ele.id}/>)
+                        }
+                        </div>
+                        :<div className="flex justify-center">
+                            <span className="inline-block w-7 h-7 rounded-full border-2 border-black border-l-gray-500 animate-spin"></span>
+                        </div>
                     }
-                </div>
+                </>
                 :<div className="text-bodyFont text-center font-semibold">You must be signed in to view the protected content on this page.</div>
             }
         </div>
